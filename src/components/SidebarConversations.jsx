ec3f84df3a18e2cc35bcfeecc37e793f7a3f2b7d
AlexisKcc8 <CodeKJS863@gmail.com>
2023-05-15 13:24:20 -0600

    componente mensajes recibidos estilizados

--- src/components/SidebarConversations.jsx
+++ src/components/SidebarConversations.jsx
@@ -1,13 +1,58 @@
+import { useEffect, useState } from "react";
+import { auth, dbFirestore } from "../firebase/firebase-config";
+
+import {
+  collection,
+  query,
+  limit,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+
 import { HeaderConversation } from "./HeaderConversation";
-import "../styles/SidebarConversation.scss";
 import { SedMessage } from "./SedMessage";
+
+import "../styles/SidebarConversation.scss";
 export const SidebarConversations = () => {
+  const [messages, setMessages] = useState([]);
+  const { userID } = auth.currentUser;
+  useEffect(() => {
+    const q = query(
+      collection(dbFirestore, "messages"),
+      orderBy("createdAt"),
+      limit(50)
+    );
+
+    const data = onSnapshot(q, (QuerySnapshot) => {
+      let messages = [];
+      QuerySnapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      console.log(messages);
+      setMessages(messages);
+    });
+
+    return () => data;
+  }, []);
   return (
     <section className="side-conversation">
       <HeaderConversation />
       <section className="container-conversation">
         <ul className="container-conversation__list">
-          <li>si</li>
+          {messages &&
+            messages.map((message, id, uid, photoURL) => (
+              <li
+                key={id}
+                className={`container-conversation__list-item ${
+                  userID === auth.currentUser.uid ? "sent" : "received"
+                }`}
+              >
+                <div className={`msg container-conversation__list-item-msg `}>
+                  {/* <img src={message.photoURL} /> */}
+                  <p>{message.text}</p>
+                </div>
+              </li>
+            ))}
         </ul>
       </section>
       <SedMessage />
